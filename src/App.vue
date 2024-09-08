b4bca5dac9dca4ea5b189756e9989491ef734cd1
bogomollov <bogdan.bogomolov.2016@mail.ru>
2024-09-08 17:44:32 +0500

    test

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import NgwMap from '@nextgis/ngw-leaflet';
+
+const webMapName = 'webmap';
+const qmsBaseLayerName = 'qmsbasemap';
+
+NgwMap.create({
+  baseUrl: "/api",
+  target: "map",
+  osm: true,
+  minZoom: 1,
+  qmsId: [448, qmsBaseLayerName],
+  resources: [ { resource: 6245, id: webMapName, useBasemap: false, fit: true }, ],
+})
+</script>
+
+<template>
+  <div id="wrapper">
+    <div id="layers"></div>
+    <div id="map"></div>
+  </div>
+</template>
